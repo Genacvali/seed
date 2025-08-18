ba4f857b06564090362d32056a3267a6880a319c
genacvali <gena12m@gmailcom>
2025-08-18 15:55:34 +0300

    update 2 files and delete 33 files

--- v2/scripts/demo_collscan.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cat <<'TXT'
-namespace                     op       time      details
-------------------------------------------------------------
-seed_demo.events              query      260ms  plan:COLLSCAN docs:200000 keys:0
-seed_demo.events              query      210ms  plan:COLLSCAN docs:200000 keys:0
-TXT
